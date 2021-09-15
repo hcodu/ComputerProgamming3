51a73123899b2d7dc0dfea20e9f7bbe6cce85c16
Harry <hcodu37@gmail.com>
2021-09-15 15:46:08 -0400

    Working floodfill program

--- src/Floodfill/Floodfill.java
+++ src/Floodfill/Floodfill.java
@@ -8,56 +8,52 @@ import java.util.Scanner;
 
 
 public class Floodfill {
-    private static char[][] filledShape;
+    private static char[][] ogShape;
 
     public static void main(String[] args) {
         boolean inside = false;
         boolean startFound = false;
-        char[][] shape;
+        char[][] shape = readIn("shape.txt");
+        ogShape = readIn("shape.txt");
 
-        shape = readIn();
         String row1, col1;
         int row, col;
-
         row1 = JOptionPane.showInputDialog("Enter row");
         col1 = JOptionPane.showInputDialog("Enter col");
         row = Integer.parseInt(row1);
         col = Integer.parseInt(col1);
 
-        inside = isInside(shape, row, col);
-        floodFill(shape, true, 3, 1);
+        inside = isInside(ogShape, row, col);
+
+        if(shape[row - 1][col - 1] == '*') {
+            System.out.print(ogShape[row][col]);
+        }
+        else if(shape[row - 1][col - 1] == '_') {
+            floodFill(shape, ogShape, inside, 1, 1);
+        }
+
         System.out.println(inside);
-
-
-
 
         for (int r = 0; r < shape.length; r++) {
             for (int c = 0; c < shape[0].length; c++) {
-                System.out.print(filledShape[r][c]);
+                System.out.print(shape[r][c]);
             }
             System.out.println("");
         }
     }
 
-        public static void floodFill (char[][] shape, boolean fillInside, int row, int col) {
-            boolean inside = isInside(shape, row, col);
+    public static void floodFill (char[][] shape, char[][] ogShape, boolean fillInside, int row, int col) {
+            boolean inside = isInside(ogShape, row, col);
             if (col >= shape[0].length || row >= shape.length) {
                 if(fillInside) { //User point is inside
                     if(inside) { //Point is inside
                         shape[row - 1][col - 1] = '*';
-                        System.out.println(row + " " + col + " is inside:" + inside + "1");
-                    }
-                    else {
-                        System.out.println(row + " " + col + " is inside:" + inside + "2");
                     }
                 }
                 if(!fillInside) {
                     if(!inside) {
                         shape[row - 1][col - 1] = '*';
-                        System.out.println(row + " " + col + " is inside:" + inside + "3");
-                    }
-                    else {
-                        System.out.println(row + " " + col + " is inside:" + inside + "4");
+                        //System.out.println(row + " " + col + " is inside:" + inside + "3");
                     }
                 }
                 return;
@@ -66,29 +62,21 @@ public class Floodfill {
                 if(fillInside) {
                     if(inside) {
                         shape[row - 1][col - 1] = '*';
-                        System.out.println(row + " " + col + " is inside:" + inside + "5");
-                    }
-                    else {
-                        System.out.println(row + " " + col + " is inside:" + inside + "6");
+                        //System.out.println(row + " " + col + " is inside:" + inside + "5");
                     }
                 }
                 if(!fillInside) {
                     if(!inside) {
                         shape[row - 1][col - 1] = '*';
-                        System.out.println(row + " " + col + " is inside:" + inside + "7");
-                    }
-                    else {
-                        System.out.println(row + " " + col + " is inside:" + inside + "8");
+                        //System.out.println(row + " " + col + " is inside:" + inside + "7");
                     }
                 }
-                floodFill(shape, fillInside, row, col + 1);
-                floodFill(shape, fillInside, row + 1, col);
+                floodFill(shape, ogShape, fillInside, row, col + 1);
+                floodFill(shape, ogShape, fillInside, row + 1, col);
             }
-            filledShape = shape;
-
         }
 
-        public static boolean isInside (char[][] shape, int row, int col){
+    public static boolean isInside (char[][] shape, int row, int col){
             if (shape[row - 1][col - 1] != '*') { // - 1 to account for index starting at 0 | determines if index is inside the shape
                 int distanceToLeft = col - 1;
                 int distanceToRight = shape[0].length - col;
@@ -103,41 +91,42 @@ public class Floodfill {
                         starCounterR++;
                 }
                 if (starCounterL > 0 && starCounterR > 0) {
+                    //System.out.println("sL " + starCounterL + " sR " + starCounterR);
                     return true;
                 }
             }
-            return false;
+        return false;
         }
 
-        public static char[][] readIn () {
-            char[][] picture;
-            int numRows, numCols, row, col;
-            // you can change this line as using Scanner to get file name or just hardcode the file name
-            String fileName = JOptionPane.showInputDialog
-                    (null, "Enter the file name:  ");
-            try {
-                Scanner file = new Scanner(new File(fileName));
-                numRows = file.nextInt();
-                numCols = file.nextInt();
+    public static char[][] readIn(String fileName) {
+        char[][] picture;
+        int numRows, numCols, row, col;
+        // you can change this line as using Scanner to get file name or just hardcode the file name
 
-                picture = new char[numRows][numCols];
+        try
+        {
+            Scanner file = new Scanner(new File(fileName));
+            numRows = file.nextInt();
+            numCols = file.nextInt();
 
-                for (int r = 0; r < picture.length; r++)
-                    for (int c = 0; c < picture[0].length; c++)
-                        picture[r][c] = '_';
+            picture = new char[numRows][numCols];
 
-                while (file.hasNext()) {
-                    row = file.nextInt();
-                    col = file.nextInt();
-                    picture[row][col] = '*';
-                }
-                file.close();
+            for(int r = 0; r < picture.length; r++)
+                for(int c = 0; c < picture[0].length; c++)
+                    picture[r][c] = '_';
 
-                return picture;
-            } catch (FileNotFoundException e) {
-                System.out.println("FloodFill: readIn(): FileNotFoundException!!");
+            while (file.hasNext()) {
+                row = file.nextInt();
+                col = file.nextInt();
+                picture[row][col]='*';
             }
+            file.close();
 
-            return null;
+            return picture;
         }
+        catch(FileNotFoundException e) {
+            System.out.println("FloodFill: readIn(): FileNotFoundException!!");
+        }
+        return null;
+    }
 }
